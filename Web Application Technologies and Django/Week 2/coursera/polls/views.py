6b4115dba8e4021e66af254548e98c082213d73a
Frankenstein2oo2 <70457661+Frankenstein2oo2@users.noreply.github.com>
2022-03-27 10:38:02 +0530

    Update views.py

--- Web Application Technologies and Django/Week 2/coursera/polls/views.py
+++ Web Application Technologies and Django/Week 2/coursera/polls/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def HomeView(request):
-    return HttpResponse("Hello, world. 78573683 is the polls index.")
+    return HttpResponse("Hello, world. 5000ab6d is the polls index.")
